a89c121491ae7a41e7e0d55af5dcb5407ce5b0f5
Collins Mwambi <77346740+CollinsMwambi@users.noreply.github.com>
2022-01-28 09:07:32 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -71,6 +71,43 @@ void display_callback()
 
 
 }
+int main( int argc, char  **argv) {
+    //initialise glut and display mode and window size
+    glutInit(&argc,argv);
+    //double buffer window
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("KENYAN SNAKE");
+    glutDisplayFunc(display_callback);
+    glutReshapeFunc(reshape_callback);
+    glutTimerFunc(0, timer_callback, 0);
+    glutSpecialFunc(keyboard_callback);
+    init();
+    glutMainLoop();
+
+    return 0;
+}
+//int index =0;
+void display_callback()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    drawGrid();
+    drawSnake();
+    drawFood();
+    glutSwapBuffers();
+    //should show message box but idk how to do that on linux yet
+    if (gameOver){
+
+        char _score[10];
+        itoa(score, _score,10);
+        char text[50] = "Your score is: ";
+        strcat(text, _score);
+        MessageBox(NULL, text, "GAME OVER");
+        exit(0);
+
+    }
+
 //used to set viewport
 void reshape_callback(int w, int h){
     glViewport(0,0,(GLsizei)w,(GLsizei)h);
@@ -106,3 +143,29 @@ void keyboard_callback(int key, int, int){
             break;
     }
 }
+    void timer_callback(int){
+    //calls display function as soon as possible
+    glutPostRedisplay();
+    glutTimerFunc(1000/FPS, timer_callback, 0);
+}
+void keyboard_callback(int key, int, int){
+    //control snake moves
+    switch (key)
+    {
+        case GLUT_KEY_UP:
+            if (sDirection!= DOWN)
+                sDirection = UP;
+            break;
+        case GLUT_KEY_DOWN:
+            if(sDirection != UP)
+                sDirection = DOWN;
+            break;
+        case GLUT_KEY_RIGHT:
+            if (sDirection != LEFT)
+                sDirection = RIGHT;
+            break;
+        case GLUT_KEY_LEFT:
+            if (sDirection != RIGHT)
+                sDirection = LEFT;
+            break;
+    }
