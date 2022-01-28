1652411bf4d4bb923e0cfe4e207afee12a3cb181
Fredrick-Rogato <61021668+Fredrick-Rogato@users.noreply.github.com>
2022-01-28 09:34:05 +0300

    Main.cpp changes

--- main.cpp
+++ main.cpp
@@ -247,3 +247,39 @@ void keyboard_callback(int key, int, int){
                 sDirection = LEFT;
             break;
     }
+
+    void reSizecallback(int w, int h) {
+        glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(0.0, COLUMNS, 0.0, ROWS, -1.0, 1.0);
+        glMatrixMode(GL_MODELVIEW);
+    }
+    void Timmek(int) {
+        //calls display function as soon as possible
+        glutPostRedisplay();
+        glutTimerFunc(1000 / FPS, timer_callback, 0);
+    }
+    void keys(int key, int, int) {
+        //control snake moves
+        switch (key)
+        {
+        case GLUT_KEY_UP:
+            if (sDirection != DOWN)
+                sDirection = UP;
+            break;
+        case GLUT_KEY_DOWN:
+            if (sDirection != UP)
+                sDirection = DOWN;
+            break;
+        case GLUT_KEY_RIGHT:
+            if (sDirection != LEFT)
+                sDirection = RIGHT;
+            break;
+        case GLUT_KEY_LEFT:
+            if (sDirection != RIGHT)
+                sDirection = LEFT;
+            break;
+        }
+    }
+    
